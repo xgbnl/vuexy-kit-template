fe82904be5bcb3bce6b36decb6bf1e661ac8231c
xgbnl <745502274@qq.com>
2025-02-28 10:59:50 +0800

    feat: next-auth声明文件

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -7,7 +7,6 @@ declare module 'next-auth' {
 }
 
 declare module 'next-auth/jwt' {
-  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
   interface JWT extends Passport {
     avatar: string
   }
