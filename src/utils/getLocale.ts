3d0e5eb6c96c983466a22ee4ef9a0df94f08ff6c
xgbnl <745502274@qq.com>
2025-03-02 14:24:50 +0800

    feat: 指定返回类型

--- src/utils/getLocale.ts
+++ src/utils/getLocale.ts
@@ -1,4 +1,7 @@
-export const getLocale = (pathName: string | null = null): string | null => {
+// Hooks Imports
+import { Locale } from '@configs/i18n'
+
+export const getLocale = (pathName: string | null = null): Locale | null => {
   const pathname: string = pathName ?? (typeof window !== 'undefined' ? window.location.pathname : '')
 
   if (!pathname) {
@@ -8,7 +11,7 @@ export const getLocale = (pathName: string | null = null): string | null => {
   const pathParts = pathname.split('/')
 
   if (pathParts.length > 1) {
-    const languageCode = pathParts[1]
+    const languageCode = pathParts[1] as Locale
 
     if (languageCode.length === 2) {
       return languageCode
