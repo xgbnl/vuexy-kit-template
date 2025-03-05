62c81d77a3b9e5a28db249081982dc918a61c421
xgbnl <745502274@qq.com>
2025-03-05 13:41:44 +0800

    chore: remove file.ts

--- src/types/elementTypes.ts
+++
@@ -1,8 +0,0 @@
-// Import react.
-import React from 'react'
-
-type Props = {
-  children?: React.ReactNode
-}
-
-export type JSXElement = <T extends Props>(props: Props = {}) => React.ReactElement;
