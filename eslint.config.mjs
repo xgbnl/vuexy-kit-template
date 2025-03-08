f07f9f3c9910ccebf23f212d62a34c13763db6e3
xgbnl <745502274@qq.com>
2025-03-08 20:46:44 +0800

    feat: 引入react-hooks规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,15 +8,19 @@ import pluginImport from 'eslint-plugin-import'
 import pluginReact from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import pluginNext from '@next/eslint-plugin-next'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     plugins: {
-      'react-hooks': reactHooks
+      'react-hooks': reactHooks,
+      '@next/next': pluginNext
     },
     rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
       'prettier/prettier': 'error',
       'jsx-a11y/alt-text': 'off',
       'react/display-name': 'off',
