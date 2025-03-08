1084dd514d9872be2e4bdde875500948099186ec
xgbnl <745502274@qq.com>
2025-03-08 16:33:36 +0800

    feat: eslint增加对prettier,reacthook支持

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,119 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+// Eslint Imports
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import { parser, configs } from 'typescript-eslint'
+
+// Plugins Imports
+import pluginImport from 'eslint-plugin-import'
+import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      'react-hooks': reactHooks
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'jsx-a11y/alt-text': 'off',
+      'react/display-name': 'off',
+      'react/no-children-prop': 'off',
+      '@next/next/no-img-element': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-empty-object-type': 'error',
+      '@typescript-eslint/no-unsafe-function-type': 'error',
+      '@typescript-eslint/no-wrapper-object-types': 'error',
+      'lines-around-comment': [
+        'error',
+        {
+          beforeBlockComment: true,
+          beforeLineComment: true,
+          allowBlockStart: true,
+          allowObjectStart: true,
+          allowArrayStart: true
+        }
+      ],
+      'padding-line-between-statements': [
+        'error',
+        {
+          blankLine: 'any',
+          prev: 'export',
+          next: 'export'
+        },
+        {
+          blankLine: 'always',
+          prev: ['const', 'let', 'var'],
+          next: '*'
+        },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var']
+        },
+        {
+          blankLine: 'always',
+          prev: '*',
+          next: ['function', 'multiline-const', 'multiline-block-like']
+        },
+        {
+          blankLine: 'always',
+          prev: ['function', 'multiline-const', 'multiline-block-like'],
+          next: '*'
+        }
+      ],
+      'newline-before-return': 'error',
+      'import/newline-after-import': [
+        'error',
+        {
+          count: 1
+        }
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', ['internal', 'parent', 'sibling', 'index'], ['object', 'unknown']],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before'
+            },
+            {
+              pattern: 'next/**',
+              group: 'external',
+              position: 'before'
+            },
+            {
+              pattern: '~/**',
+              group: 'external',
+              position: 'before'
+            },
+            {
+              pattern: '@/**',
+              group: 'internal'
+            }
+          ],
+          pathGroupsExcludedImportTypes: ['react', 'type'],
+          'newlines-between': 'always-and-inside-groups'
+        }
+      ]
+    }
+  },
   {
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
-      parser:tseslint.parser,
-      parserOptions:{
-        project: 'tsconfig.json',
-        sourceType:'module'
+      parser: parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module'
       }
     }
   },
@@ -21,10 +122,21 @@ export default [
     settings: {
       react: {
         version: 'detect'
+      },
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx']
+      },
+      'import/resolver': {
+        node: {},
+        typescript: {
+          project: './tsconfig.json'
+        }
       }
     }
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...configs.recommended,
   pluginReact.configs.flat.recommended,
-];
+  pluginImport.flatConfigs.recommended,
+  pluginPrettierRecommended
+]
