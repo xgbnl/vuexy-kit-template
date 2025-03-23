3575b7c6f05223cd5c01093fa6a2f7692c3b5b9a
xgbnl <745502274@qq.com>
2025-03-24 06:35:45 +0800

    chore: 替换Math.random为crypt，兼容客户端与服务端，避免出现水合现象

--- src/utils/uuid.ts
+++ src/utils/uuid.ts
@@ -1,7 +1,13 @@
+// Node Imports
+import { randomBytes } from 'crypto'
+
 export const uuid = (): string => {
-  return 'xxxx-xxxx-4xxx-yxxx-xxxx-xxxx'.replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0,
-      v = c === 'x' ? r : (r & 0x3) | 0x8
+  const bytes = randomBytes(16)
+
+  return 'xxxx-xxxx-4xxx-yxxx-xxxx-xxxx'.replace(/[xy]/g, function (c, index) {
+    const byte = bytes[index % 16]
+    const r = byte & 0xf
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
 
     return v.toString(16)
   })
