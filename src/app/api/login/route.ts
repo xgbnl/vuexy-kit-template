f72a04ba4de87ea30877c2f4b2c79574a1f64071
xgbnl <745502274@qq.com>
2025-04-03 15:01:33 +0800

    refactor: 重写登录路由.

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,44 +1,38 @@
 // Next Imports
 import { NextResponse } from 'next/server'
 
-// Utils Imports
-import { getAppUrl } from '@/utils/getAppUrl'
-
 // Configs Imports
-import type { JsonResponse } from '@/configs/fetch'
+import type { JsonResponse, Throwable } from '@/configs/fetch'
 
 // Libs Imports
-import { Get } from '@/libs/fetch/next'
+import { Post } from '@/libs/fetch/next'
 import type { Authenticatable } from '@/libs/auth/types'
 
 export async function POST(req: Request) {
   // Vars
   const credentials = await req.json()
 
-  let promise: Response
+  let response: JsonResponse<Authenticatable>
 
   try {
-    promise = await Get(getAppUrl(String(process.env.NEXT_PUBLIC_API_AUTH)))
-    console.log(promise)
+    response = await Post<JsonResponse<Authenticatable>>(String(process.env.NEXT_PUBLIC_API_AUTH), credentials)
   } catch (e) {
-    const error = e instanceof TypeError ? e.message : (e as string)
+    const error = e as Throwable
 
     return NextResponse.json({
-      code: 500,
-      msg: `[${error}] Please check whether the server is running normally`,
+      code: error.code,
+      msg: error.msg,
       data: null
     })
   }
 
-  if (promise.status !== 200 && !promise.ok) {
+  if (response.code !== 200) {
     return NextResponse.json({
-      code: 500,
-      msg: 'Unable to connect to server',
+      code: response.code,
+      msg: response.msg,
       data: null
     })
   }
 
-  const jsonResponse: JsonResponse<Authenticatable> = await promise.json()
-
-  return NextResponse.json(jsonResponse)
+  return NextResponse.json(response)
 }
