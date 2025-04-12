e906865b36ffa457ba56808a90e392456cc18abe
xgbnl <745502274@qq.com>
2025-04-12 16:46:20 +0800

    chore: change Provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -7,20 +7,23 @@ import { VerticalNavProvider } from '@menu/contexts/verticalNavContext'
 import { SettingsProvider } from '@core/contexts/settingsContext'
 import ThemeProvider from '@components/theme'
 import AppReactToastify from '@/libs/styles/AppReactToastify'
+import DatepickerLocalizationProvider from '@/hocs/DatepickerLocalizationProvider'
 
 // Util Imports
 import { getMode, getSettingsFromCookie, getSystemMode } from '@core/utils/serverHelpers'
 
 // Redux Imports
 import ReduxStoreProvider from '@/hocs/ReduxStoreProvider'
+import type { Locale } from '@/configs/i18n'
 
 type Props = ChildrenType & {
   direction: Direction
+  lang: Locale
 }
 
 const Providers = async (props: Props) => {
   // Props
-  const { children, direction } = props
+  const { children, direction, lang } = props
 
   // Vars
   const mode = await getMode()
@@ -32,7 +35,9 @@ const Providers = async (props: Props) => {
       <VerticalNavProvider>
         <SettingsProvider settingsCookie={settingsCookie} mode={mode}>
           <ThemeProvider direction={direction} systemMode={systemMode}>
-            <ReduxStoreProvider>{children}</ReduxStoreProvider>
+            <ReduxStoreProvider>
+              <DatepickerLocalizationProvider lang={lang}>{children}</DatepickerLocalizationProvider>
+            </ReduxStoreProvider>
             <AppReactToastify direction={direction} hideProgressBar />
           </ThemeProvider>
         </SettingsProvider>
