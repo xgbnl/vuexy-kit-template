0c63c47aa8e65d93157fa700c3ed190a510e6743
xgbnl <745502274@qq.com>
2025-04-12 16:46:39 +0800

    feat: custom Pickers.

--- src/app/[lang]/[...not-found]/page.tsx
+++ src/app/[lang]/[...not-found]/page.tsx
@@ -6,14 +6,18 @@ import NotFound from '@views/NotFound'
 // Util Imports
 import { getServerMode, getSystemMode } from '@core/utils/serverHelpers'
 
-const NotFoundPage = async () => {
+// Types Imports
+import type { Locale } from '@/configs/i18n'
+
+const NotFoundPage = async (props: { params: Promise<{ lang: Locale }> }) => {
   // Vars
   const direction = 'ltr'
   const mode = await getServerMode()
   const systemMode = await getSystemMode()
+  const params = await props.params
 
   return (
-    <Providers direction={direction}>
+    <Providers direction={direction} lang={params.lang}>
       <BlankLayout systemMode={systemMode}>
         <NotFound mode={mode} />
       </BlankLayout>
--- src/views/apps/date-pickers/PickersBetween.tsx
+++ src/views/apps/date-pickers/PickersBetween.tsx
@@ -1,66 +1,34 @@
-// React Imports
-import { useState, forwardRef } from 'react'
-
 // MUI Imports
-import type { TextFieldProps } from '@mui/material/TextField'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 
-// Third-party Imports
-import { format, addDays } from 'date-fns'
-import { zhCN } from 'date-fns/locale'
-
-// Component Imports
-import AppReactDatepicker from '@/libs/styles/AppReactDatepicker'
-import CustomTextField from '@core/components/mui/TextField'
-
-type CustomInputProps = TextFieldProps & {
-  label: string
-  end: Date | number
-  start: Date | number
+type Props = {
+  inputProps?: {
+    label: {
+      start: string
+      end: string
+    }
+  }
 }
 
-const PickersRange = () => {
+export default function BasicDateRangePicker(props: Props) {
   // States
-  const [startDateRange, setStartDateRange] = useState<Date | null | undefined>(new Date())
-  const [endDateRange, setEndDateRange] = useState<Date | null | undefined>(addDays(new Date(), 45))
-
-  const handleOnChangeRange = (dates: any) => {
-    const [start, end] = dates
-
-    setStartDateRange(start)
-    setEndDateRange(end)
-  }
-
-  const CustomInput = forwardRef((props: CustomInputProps, ref) => {
-    const { label, start, end, ...rest } = props
-
-    const startDate = format(start, 'yyyy-MM-dd')
-    const endDate = end !== null ? ` 至 ${format(end, 'yyyy-MM-dd')}` : null
-
-    const value = `${startDate}${endDate !== null ? endDate : ''}`
-
-    return <CustomTextField fullWidth inputRef={ref} {...rest} label={label} value={value} />
-  })
+  const { inputProps } = props
 
   return (
-    <AppReactDatepicker
-      selectsRange
-      locale={zhCN}
-      monthsShown={2}
-      endDate={endDateRange as Date}
-      selected={startDateRange}
-      startDate={startDateRange as Date}
-      shouldCloseOnSelect={false}
-      id='date-range-picker-months'
-      onChange={handleOnChangeRange}
-      customInput={
-        <CustomInput
-          label='Multiple Months'
-          end={endDateRange as Date | number}
-          start={startDateRange as Date | number}
-        />
-      }
-    />
+    <div className='flex justify-between'>
+      <DatePicker
+        format='yyyy-MM-dd'
+        label={inputProps?.label.start ?? 'Start Date'}
+        slotProps={{ textField: { size: 'small' } }}
+      />
+      <DatePicker
+        slotProps={{ textField: { size: 'small' } }}
+        format='yyyy-MM-dd'
+        label={inputProps?.label.end ?? 'End Date'}
+        onChange={newValue => {
+          console.log(newValue)
+        }}
+      />
+    </div>
   )
 }
-
-export default PickersRange
